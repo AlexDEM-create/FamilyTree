8765394f9872ca0387b03067b1c36a8d677c88ab
alexd <1090424@gmail.com>
2024-04-07 15:37:05 +0300

    Ещё не начал править конструктор

---
+++ src/NumerateHuman.java
@@ -0,0 +1,13 @@
+public class NumerateHuman {
+    private int id;
+    private static int humancount = 0;
+
+    public NumerateHuman(){
+        humancount++;
+        id = humancount;
+  }
+  public int gethumanId() {
+      return this.id;
+
+  }
+}
